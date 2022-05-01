8ffe7bdba0b3154c49a89ad3658d318ca511337b
0-gain <1596861254@qq.com>
2022-05-01 23:47:53 +0800

    后台管理

---
+++ vue-admin-template-master/src/api/index.js
@@ -0,0 +1,9 @@
+export { default as trademark } from './product/trademark'
+export { default as attr } from './product/attr'
+export { default as spu } from './product/spu'
+export { default as sku } from './product/sku'
+export { default as category } from './product/category'
+/* import { default as trademark } from './product/trademark'
+export {
+  trademark
+} */
